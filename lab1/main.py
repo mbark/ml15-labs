237113c015c74f740552b1d16534d2294f7f54ec
mbark <barksten@kth.se>
2015-09-02 14:38:01 +0200

    Print as tables

--- lab1/main.py
+++ lab1/main.py
@@ -2,18 +2,24 @@ import monkdata as m
 import dtree
 
 def assignment1():
-	print("monk1", dtree.entropy(m.monk1))
-	print("monk2", dtree.entropy(m.monk2))
-	print("monk3", dtree.entropy(m.monk3))
+	print "   ", "Entropy"
+	print "M1 ", dtree.entropy(m.monk1)
+	print "M2 ", dtree.entropy(m.monk2)
+	print "M3 ", dtree.entropy(m.monk3)
 
-def avgForDataset(dataset, name):
+def avgForDataset(dataset):
+	res = "";
 	for attr in m.attributes:
-		print(name, attr.name, dtree.averageGain(dataset, attr))
+		res += "{:.5f}".format(dtree.averageGain(dataset, attr))
+		res += " "
+	return res
 
 def assignment2():
-	avgForDataset(m.monk1, "monk1")
-	avgForDataset(m.monk2, "monk2")
-	avgForDataset(m.monk3, "monk3")
+	print "   ", "     a1", "     a2", "     a3" , "     a4", "     a5", "     a6"
+	print "M1 ", avgForDataset(m.monk1)
+	print "M2 ", avgForDataset(m.monk2)
+	print "M3 ", avgForDataset(m.monk3)
 
 assignment1()
+print
 assignment2()
