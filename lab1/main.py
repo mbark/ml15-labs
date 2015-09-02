7202f73650e7d7371325ae334aef9f6244bdbb2b
mbark <barksten@kth.se>
2015-09-02 14:48:24 +0200

    Assignment 3?

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,16 @@
 import monkdata as m
-import dtree
+import dtree as d
 
 def assignment1():
 	print "   ", "Entropy"
-	print "M1 ", dtree.entropy(m.monk1)
-	print "M2 ", dtree.entropy(m.monk2)
-	print "M3 ", dtree.entropy(m.monk3)
+	print "M1 ", d.entropy(m.monk1)
+	print "M2 ", d.entropy(m.monk2)
+	print "M3 ", d.entropy(m.monk3)
 
 def avgForDataset(dataset):
 	res = "";
 	for attr in m.attributes:
-		res += "{:.5f}".format(dtree.averageGain(dataset, attr))
+		res += "{:.5f}".format(d.averageGain(dataset, attr))
 		res += " "
 	return res
 
@@ -20,6 +20,18 @@ def assignment2():
 	print "M2 ", avgForDataset(m.monk2)
 	print "M3 ", avgForDataset(m.monk3)
 
+def checkAndBuild(dataset, compare):
+	t = d.buildTree(dataset, m.attributes);
+	return d.check(t, compare)
+
+def assignment3():
+	print "   ", "  train", "   test"
+	print "M1 ", "{:.5f}".format(checkAndBuild(m.monk1, m.monk1)), "{:.5f}".format(checkAndBuild(m.monk1, m.monk1test))
+	print "M2 ", "{:.5f}".format(checkAndBuild(m.monk2, m.monk2)), "{:.5f}".format(checkAndBuild(m.monk2, m.monk2test))
+	print "M3 ", "{:.5f}".format(checkAndBuild(m.monk3, m.monk3)), "{:.5f}".format(checkAndBuild(m.monk3, m.monk3test))
+
 assignment1()
 print
 assignment2()
+print
+assignment3()
