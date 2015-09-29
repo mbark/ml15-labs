3b9544c29655f4f18a309615e91f71174540bbae
Sandra Grosz <sgrosz@kth.se>
2015-09-29 16:44:44 +0200

    Added the saved figures of each assignment

--- lab3/main.m
+++ lab3/main.m
@@ -85,3 +85,15 @@ g = adaboost_discriminant([z1 z2], mu, sigma, p, alpha, classes, T);
 gg = reshape(g, length(y), length(x));
 [c,h] = contour(x, y, gg, [0.5 0.5]);
 set(h, 'LineWidth', 3);
+
+tmp = reshape(book_rg, size(book_rg,1)*size(book_rg,2), 2);
+g = adaboost_discriminant(tmp, mu, sigma, p, alpha, classes, T);
+gg = reshape(g, size(book_rg,1), size(book_rg,2));
+mask = gg > 0.5;
+
+mask3D(:,:,1) = mask;
+mask3D(:,:,2) = mask;
+mask3D(:,:,3) = mask;
+result_im = uint8(double(book) .* mask3D);
+figure;
+imagesc(result_im);
